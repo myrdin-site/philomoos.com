3add0887cf55a82a41f6801e970262b5c9a831c2
Myrdin <myrdin@clic2000.fr>
2021-03-11 17:30:43 +0100

    Disable purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-      enabled: true,
+      enabled: false, // change at true for production
       content: ['./dist/**/*.html'],
     },
   darkMode: false, // or 'media' or 'class'
