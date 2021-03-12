6f7dc41f7541d187f7199a67cb19635c6caa70d7
Myrdin <myrdin@clic2000.fr>
2021-03-12 19:34:18 +0100

    Add javascript to toggle active class

---
+++ dist/assets/js/script.js
@@ -0,0 +1,10 @@
+// Navigation on mobile
+
+let hamburger = document.querySelector("#hamburger");
+let mobileMenu = document.querySelector("#menu-mobile");
+
+hamburger.addEventListener('click', function(){
+    mobileMenu.classList.toggle('active');
+    });
+
+
