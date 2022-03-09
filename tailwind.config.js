8a643744759c42e8b588d6f85a906f8f3aaf84c3
myrdn <43375226+myrdn@users.noreply.github.com>
2022-03-09 12:08:37 +0000

    Deploying to gh-pages from @ clic2000/philomoos.com@2f269493c62e6130e730131b181fa8732a104727 🚀

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  purge: {
-      enabled: false, // change at true for production
-      content: ['*.html'],
-    },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    '*.html',
+  ],
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
 }
